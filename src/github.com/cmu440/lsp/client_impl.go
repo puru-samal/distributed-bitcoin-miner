9b635711d0443dffee81e5407fa2b9238190b5b0
Puru Samal <puruboii@gmail.com>
2024-09-20 05:24:14 -0400

    NewClient success

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,44 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/cmu440/lspnet"
+)
+
+type ClientState int
+
+const (
+	Connect ClientState = iota // Connect State
+	Active                     // Active State
+)
 
 type client struct {
-	// TODO: implement this!
+	// general
+	state      ClientState
+	serverAddr *lspnet.UDPAddr
+	clientConn *lspnet.UDPConn
+
+	// NewClient
+	closeNewClient chan bool
+
+	connID        int
+	getConnID     chan int
+	params        *Params
+	counter       int
+	updateCounter chan int
+	closeConn     chan int
+	msgSendChan   chan *Message
+	msgRecvChan   chan *Message
+	msgMap        map[int]*Message // map of UnackedMessages
+	mapLB         int              // Lower bound (inclusive)
+	mapUB         int              // Upper bound (exclusive)
+	maxSize       int              // initially 1 (Connect), then MaxUnackedMessages
+	currSn        int
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -22,7 +56,56 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, initialSeqNum int, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+
+	// Create a connection
+	serverAddr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		fmt.Println("Error: serverAddr un-resolved")
+		return nil, err
+	}
+
+	conn, err := lspnet.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		fmt.Println("Error: Could not connect")
+		return nil, err
+	}
+
+	c := &client{
+		state:          Connect,
+		serverAddr:     serverAddr,
+		clientConn:     conn,
+		closeNewClient: make(chan bool),
+		connID:         0,
+		getConnID:      make(chan int),
+		params:         params,
+		counter:        0,
+		updateCounter:  make(chan int),
+		closeConn:      make(chan int),
+		msgSendChan:    make(chan *Message),
+		msgRecvChan:    make(chan *Message),
+		msgMap:         make(map[int]*Message),
+		mapLB:          initialSeqNum,
+		mapUB:          initialSeqNum + 1,
+		maxSize:        1,
+		currSn:         0,
+	}
+
+	// Launch Main Routine
+	// Launch Read Routine
+	// Launch Epoch Timer
+	go c.main()
+	go c.reader()
+	go c.epochTimer()
+
+	// Signal main to send NewConnect message
+	// Block until connID recv or EpochLimit reached
+	c.msgSendChan <- NewConnect(initialSeqNum)
+	status := <-c.closeNewClient
+	if !status {
+		return nil, errors.New("connection could not be established")
+	}
+	fmt.Println("Success: Connected!")
+	return c, nil
 }
 
 func (c *client) ConnID() int {
@@ -41,4 +124,137 @@ func (c *client) Write(payload []byte) error {
 
 func (c *client) Close() error {
 	return errors.New("not yet implemented")
+}
+
+func (c *client) main() {
+	for {
+		select {
+		case <-c.closeConn:
+			return
+		case msg := <-c.msgSendChan:
+			// Check if msg allowable by sliding window protocol
+			// Check if mapSize < (1 (Connect) || MaxUnackedMessages)
+			// ^ If yes send message,
+			fmt.Printf("message to send: %s\n", msg)
+			if c.mapLB <= msg.SeqNum && msg.SeqNum < c.mapUB {
+				if len(c.msgMap) < c.maxSize {
+
+					// Send message
+					// If Connect request or Data message
+					// Add to map tracking unAck's messages
+
+					fmt.Printf("sending message: %s\n", msg)
+
+					byt, err := json.Marshal(msg)
+					if err == nil {
+						c.clientConn.Write(byt)
+					}
+
+					switch msg.Type {
+					case MsgConnect, MsgData:
+						c.msgMap[msg.SeqNum] = msg
+						c.currSn++
+					default:
+						fmt.Println("Unhandled: MsgAck, MsgCAck")
+					}
+
+				}
+			}
+		case msg := <-c.msgRecvChan:
+			fmt.Printf("recv message: %s\n", msg)
+			switch c.state {
+			case Connect:
+				if msg.ConnID != 0 {
+					_, exist := c.msgMap[msg.SeqNum]
+					if exist {
+						delete(c.msgMap, msg.SeqNum)
+						c.connID = msg.ConnID
+						c.state = Active
+						c.mapLB = c.mapLB + 1
+						c.mapUB = c.mapLB + c.params.WindowSize
+						c.maxSize = c.params.MaxUnackedMessages
+						c.closeNewClient <- true
+					}
+				}
+
+			case Active:
+				switch msg.Type {
+				case MsgData:
+					// send Ack to msgChan
+					fmt.Println("Unhandled: MsgData")
+				case MsgAck, MsgCAck:
+					//fmt.Println("Unhandled: MsgAck, MsgCAck")
+					c.msgSendChan <- msg
+				}
+			}
+
+		case <-c.updateCounter: // Get Epoch tick
+			c.counter++
+			fmt.Println(".")
+			switch c.state {
+			case Connect:
+				// If epoch limit is hit, drop connection
+				if c.counter == c.params.EpochLimit {
+					c.closeNewClient <- false
+				}
+			case Active:
+			}
+
+		}
+	}
+
+}
+
+func (c *client) reader() {
+	for {
+		select {
+		// handle termination due to server Close
+		case <-c.closeConn:
+			return
+		default:
+			buf := make([]byte, 2000)
+			n, err := c.clientConn.Read(buf)
+			if err == nil {
+				var msg Message
+				json.Unmarshal(buf[:n], &msg)
+				// Check integrity
+				if checkIntegrity(&msg) {
+					fmt.Printf("read message: %s\n", &msg)
+					c.msgRecvChan <- &msg
+				}
+			}
+		}
+	}
+}
+
+func (c *client) epochTimer() {
+	timer := time.NewTicker(time.Duration(c.params.EpochMillis) * time.Millisecond)
+	for {
+		select {
+		case <-c.closeConn:
+			return
+		case <-timer.C: // Epoch tick
+			c.updateCounter <- 1
+		}
+	}
+}
+
+func checkIntegrity(msg *Message) bool {
+	switch msg.Type {
+	case MsgData:
+		if len(msg.Payload) < msg.Size {
+			return false
+		}
+		msg.Payload = msg.Payload[:msg.Size]
+		return msg.Checksum == CalculateChecksum(msg.ConnID, msg.SeqNum, msg.Size, msg.Payload)
+	default:
+		return true
+	}
+}
+
+// Assert function
+func assert(condition bool, message string) {
+	if !condition {
+		panic(message)
+	}
 }
